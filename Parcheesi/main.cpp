e7ec7d4065be3a83d5ed62bbed555a49493f1004
Bernardo Botelho <Bernardo@orion.local>
2012-10-15 21:21:25 -0400

    Initial commit.

--- Parcheesi/main.cpp
+++ Parcheesi/main.cpp
@@ -1,18 +1,4 @@
-//
-//  main.cpp
-//  Parcheesi
-//
-//  Created by Bernardo Botelho on 10/15/12.
-//  Copyright (c) 2012 Computer Graphics. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, char *argv) {
+    
     return 0;
 }
-
