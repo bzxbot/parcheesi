f087e9429b9c1e8e23b38e38586e94e3648ccf4e
Nicholas M. Mizoguchi <ichnich@gmail.com>
2012-10-22 02:04:27 -0400

    Pawn Design
    To draw a pawn, set the color, size/position (viewport) and call the
    displayList.

--- Design/board.cpp
+++ Design/board.cpp
@@ -7,6 +7,10 @@
 #define screenWidth 800
 #define screenHeight 600
 
+#define PI 3.14159265
+
+GLuint pawnPlus, pawnMinus, pawnL, pawnOnly;
+
 //<<<<<<<<<<<<<<<<<<<<<<<< Create Canvas >>>>>>>>>>>
 Canvas cvs(screenWidth, screenHeight, "Parcheesi v1.0");
 
@@ -191,7 +195,7 @@ void drawSafeSpots(){
 	drawSingleSpot(r,g,b);
 }
 
-//<<<<<<<<<<<<<<<<<<<<<<<< drawTable >>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<< drawTable >>>>>>>>>>>>>>>
 void drawTable(){
 	cvs.setWindow(-0.01,24.0,0.0,24.01);
 	drawSafeSpots();
@@ -230,15 +234,123 @@ void drawTable(){
 	drawCentral();
 }
 
+//<<<<<<<<<<<<<<<<<<<<<<<< drawCircle >>>>>>>>>>>>>>
+void generatePawnDLists(){
+	int x = 0, y = 0, r = 10;
+
+	/*********** pawnOnly **************/
+	pawnOnly = glGenLists(1);
+	glNewList(pawnOnly, GL_COMPILE);
+	cvs.setWindow(-10.0, 10, -10.0, 10.0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glLineWidth(1.0);
+	glBegin(GL_POLYGON);
+		for(GLfloat angle = 0; angle <= 360; angle+=360/50){ // Draw the Circle
+			// Calculate and position each vertex
+			glVertex2f(x+r*cos(angle*PI/180),y+r*sin(angle*PI/180));
+		}
+	glEnd();
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glColor3f(0,0,0);
+	// ----------- Begin drawing the Circle with TriangleFans
+    glBegin(GL_POLYGON);
+		for(GLfloat angle = 0; angle <= 360; angle+=360/50){ // Draw the Circle
+			// Calculate and position each vertex
+			glVertex2f(x+r*cos(angle*PI/180),y+r*sin(angle*PI/180));
+		}
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+		for(GLfloat angle = -180; angle <= 70; angle+=360/50){ // Draw the Circle
+			// Calculate and position each vertex
+			glVertex2f(x+r*0.8*cos(angle*PI/180),y+r*0.8*sin(angle*PI/180));
+		}
+	glEnd();
+	glEndList();
+
+	/*********** pawnPlus **************/
+	pawnPlus = glGenLists(1);
+	glNewList(pawnPlus, GL_COMPILE);
+		glCallList(pawnOnly);
+		// Draw Plus
+		glLineWidth(2.0);
+		glBegin(GL_LINES);
+			glVertex2f(-5,0);
+			glVertex2f(5,0);
+			glVertex2f(0,-5);
+			glVertex2f(0,5);
+		glEnd();
+		glLineWidth(1.0);
+	glEndList();
+
+	/*********** pawnMinus **************/
+	pawnMinus = glGenLists(1);
+	glNewList(pawnMinus, GL_COMPILE);
+		glCallList(pawnOnly);
+		// Draw Plus
+		glLineWidth(2.0);
+		glBegin(GL_LINES);
+			glVertex2f(-5,0);
+			glVertex2f(5,0);
+		glEnd();
+		glLineWidth(1.0);
+	glEndList();
+
+	/*********** pawnL *****************/
+	pawnL = glGenLists(1);
+	glNewList(pawnL, GL_COMPILE);
+		glCallList(pawnOnly);
+		// Draw L
+		glLineWidth(2.0);
+		glBegin(GL_LINES);
+			glVertex2f(0,-5);
+			glVertex2f(0,5);
+		glEnd();
+		glLineWidth(1.0);
+	glEndList();
+}
+
 //<<<<<<<<<<<<<<<<<<<<<<<< display >>>>>>>>>>>>>>>>>
 void display(void)
 {
 	cvs.clearScreen();
 	cvs.setBackgroundColor(1.0, 1.0, 1.0);
 	cvs.setColor(1.0, 1.0, 1.0);
-	cvs.setWindow(0, 800, 0.0, 800.0);
-	cvs.setViewport(0, 800, 0, 600);
 	drawTable();
+
+	// Draw Little Pawns
+	glColor3f(1,0,0);
+	glViewport(0, 0, 20, 20);
+	glCallList(pawnOnly);
+	
+	glColor3f(1,0,0);
+	glViewport(30, 0, 20, 20);
+	glCallList(pawnPlus);
+	
+	glColor3f(1,0,0);
+	glViewport(60, 0, 20, 20);
+	glCallList(pawnMinus);
+
+	glColor3f(1,0,0);
+	glViewport(90, 0, 20, 20);
+	glCallList(pawnL);
+	
+	// Draw Big Pawns
+	glColor3f(1,0,0);
+	glViewport(10, 300, 50, 50);
+	glCallList(pawnOnly);
+	
+	glColor3f(1,0,0);
+	glViewport(10, 230, 50, 50);
+	glCallList(pawnPlus);
+	
+	glColor3f(1,0,0);
+	glViewport(80, 230, 50, 50);
+	glCallList(pawnMinus);
+
+	glColor3f(1,0,0);
+	glViewport(80, 300, 50, 50);
+	glCallList(pawnL);
+	
 	glFlush();
 }
 
@@ -246,6 +358,7 @@ void display(void)
 int main(int argc, char** argv) {
 	cvs.setBackgroundColor(1.0, 1.0, 1.0);
 	// Window is opened in the Canvas constructor
+	generatePawnDLists();
 	glutDisplayFunc(display);
 	glutMainLoop();
 	return 0;
